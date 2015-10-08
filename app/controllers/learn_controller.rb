9eeafd3e7951e3863170bcc6fd0a1c315de9dc08
punst0531 <punst0531@gmail.com>
2015-10-08 06:33:13 +0000

    first commit

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,4 +1,10 @@
 class LearnController < ApplicationController
   def index
   end
+  
+  def create
+      Talk.create(usertalk: params[:ask], bottalk: params[:answer])
+      redirect_to '/learn'
+  end
+  
 end
--- app/controllers/talker_controller.rb
+++ app/controllers/talker_controller.rb
@@ -1,4 +1,18 @@
 class TalkerController < ApplicationController
   def index
+    @chats = Chat.all
+  end
+  
+  def create
+     talk = Talk.where(usertalk: params[:ask]).sample
+
+   unless talk.nil?
+      Chat.create(chat_type: "user", chat_content: talk.usertalk)
+      Chat.create(chat_type: "bot", chat_content: talk.bottalk)
+      redirect_to '/'
+   else
+      redirect_to '/learn'
+   end
+
   end
 end
